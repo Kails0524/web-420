5cf065688883ffd8ddac8d017ea46196a78a605e
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-09-04 18:21:18 -0500

    Update stephens-composer.js

--- models/stephens-composer.js
+++ models/stephens-composer.js
@@ -1,1 +1,18 @@
-txt
+/*
+; Title: stephens-composer.js
+; Author: Kailee Stephens
+; Date: 4 Sept 2022
+; Description: Assignment 4.2 - Composer API
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema for the composer
+let composerSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+// Exporting the composer model
+module.exports = mongoose.model("Composer", composerSchema);
