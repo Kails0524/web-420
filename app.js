368e7c01d41dc8f1f79b41052fe519493bb2733a
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-10-02 15:02:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 /*
  Title: app.js
- Author: Professor Krasso
+ Author: Kailee Stephens
  Date: 08/14/2022
- Modified By: Kailee Stephens
  Description: Intro to RESTful APIs
 */
 
@@ -10,18 +9,30 @@ const express = require("express");
 const http = require("http");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+
+// swaggerDocument = yaml.load("./docs/stephens-composers.yaml");
 const mongoose = require("mongoose");
-const composerAPI = require("./routes/stephens-composer-routes");
-const personAPI = require("./routes/stephens-person-routes")
+
+const composerAPI = require("./routes/stephens-composer-routes.js");
+const personAPI = require("./routes/stephens-person-routes.js");
+const userAPI = require("./routes/stephens-session-routes.js");
 
 /*initialize express*/
-var app = express();
+const app = express();
 
 /* Set app port to connect to port 3000  */
 app.set("port", process.env.PORT || 3000);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+var CONN ='mongodb+srv://web420_user:s3cret@buwebdev-cluster-1.lsdxy.mongodb.net/web420DB?retryWrites=true&w=majority';
+mongoose.connect(CONN).then(() => {
+  console.log('Connection to MongoDB database was successful');
+}).catch(err => {
+  console.log('MongoDB Error: ' + err.message);
+});
 
 /* Doc: define an object literal named options with the following properties/values*/
 const options = {
@@ -39,12 +50,14 @@ const options = {
 const openapiSpecification = swaggerJsdoc(options);
 /* Doc: wire the openapiSpecification variable to the app variable. Configure express to use /api-docs route to serve swaggerJsdoc  */
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
-app.use('/api', composerAPI);
-app.use('/api', personAPI);
+app.use('/api', composerAPI); //yes
 
-/* Doc: use the http library to create a new server that listens on the port 3000  */
+
+
 http.createServer(app).listen(app.get("port"), function () {
   console.log(`Application started and listening on port ${app.get("port")}`);
 });
+
+
 
 
